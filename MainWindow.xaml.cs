e8b7463c141a2eadc234592c4f43d21636502555
Danil Dmitrievich <132708222+ispbdd@users.noreply.github.com>
2024-11-01 15:01:21 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+
+namespace Glad_L3Z5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Чтение начальной суммы вклада и целевых значений из текстовых полей
+                double initialDeposit = double.Parse(InitialDepositTextBox.Text);
+                double increaseThreshold = double.Parse(IncreaseThresholdTextBox.Text);
+                double targetAmount = double.Parse(TargetAmountTextBox.Text);
+
+                double deposit = initialDeposit;
+                double monthlyIncrease = 0;
+                int month = 0;
+                int monthWhenIncreaseExceedsThreshold = 0;
+                int monthWhenDepositExceedsTarget = 0;
+
+                while (true)
+                {
+                    month++;
+                    monthlyIncrease = deposit * 0.02;
+                    deposit += monthlyIncrease;
+
+                    if (monthWhenIncreaseExceedsThreshold == 0 && monthlyIncrease > increaseThreshold)
+                    {
+                        monthWhenIncreaseExceedsThreshold = month;
+                    }
+
+                    if (monthWhenDepositExceedsTarget == 0 && deposit > targetAmount)
+                    {
+                        monthWhenDepositExceedsTarget = month;
+                        break;
+                    }
+                }
+
+                ResultLabelGrowth.Content = $"а) Месяц, когда увеличение превысит {increaseThreshold} руб.: {monthWhenIncreaseExceedsThreshold}";
+                ResultLabelAmount.Content = $"б) Месяцев до вклада более {targetAmount} руб.: {monthWhenDepositExceedsTarget}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.");
+            }
+        }
+    }
+}
