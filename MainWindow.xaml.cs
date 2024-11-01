00ca0d0028061534f1c4063e3659463b24e98e5a
Danil Dmitrievich <132708222+ispbdd@users.noreply.github.com>
2024-11-01 15:08:00 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Glad_L3Z16
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FindUniqueDigitNumbers_Click(object sender, RoutedEventArgs e)
+        {
+            var result = FindUniqueDigitNumbers();
+            ResultText.Text = result;
+        }
+
+        private string FindUniqueDigitNumbers()
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 100; i <= 999; i++)
+            {
+                string strNum = i.ToString();
+                if (HasUniqueDigits(strNum))
+                {
+                    result.Append(strNum + " ");
+                }
+            }
+            return result.ToString().Trim();
+        }
+
+        private bool HasUniqueDigits(string number)
+        {
+            var digits = new HashSet<char>();
+            foreach (char c in number)
+            {
+                if (digits.Contains(c))
+                {
+                    return false;
+                }
+                digits.Add(c);
+            }
+            return true;
+        }
+    }
+}
