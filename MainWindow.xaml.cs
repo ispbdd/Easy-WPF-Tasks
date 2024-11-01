e6898ef7483e1597deceee8677c3ac6870d235af
Danil Dmitrievich <132708222+ispbdd@users.noreply.github.com>
2024-11-01 15:06:46 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace Glad_L3Z14
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateSecond_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(InputN.Text, out int n) && n >= 1) // Убедитесь, что введено натуральное число
+            {
+                long result2 = CalculateSumOfIdenticalPowers(n);
+                ResultText2.Text = result2.ToString();
+            }
+            else
+            {
+                ResultText2.Text = "Некорректный ввод.";
+            }
+        }
+
+        private long CalculateSumOfIdenticalPowers(int n)
+        {
+            long sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += (long)Math.Pow(i, i);
+            }
+            return sum;
+        }
+    }
+}
