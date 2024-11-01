c7140e53b9a5dd85655810bc5c70c34157b9c918
Danil Dmitrievich <132708222+ispbdd@users.noreply.github.com>
2024-11-01 15:07:22 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+
+namespace Glad_L3Z15
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FindThreeDigitNumbers_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(InputN.Text, out int n) && n <= 27)
+            {
+                var result3 = FindThreeDigitNumbers(n);
+                ResultText3.Text = result3;
+            }
+            else
+            {
+                ResultText3.Text = "Некорректный ввод или превышено значение n.";
+            }
+        }
+
+        private string FindThreeDigitNumbers(int n)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 100; i <= 999; i++)
+            {
+                int sumOfDigits = SumDigits(i);
+                if (sumOfDigits == n)
+                {
+                    result.Append(i + " ");
+                }
+            }
+            return result.ToString().Trim();
+        }
+
+        private int SumDigits(int number)
+        {
+            int sum = 0;
+            foreach (char c in number.ToString())
+            {
+                sum += c - '0';
+            }
+            return sum;
+        }
+    }
+}
